b646ff6dc676cebc84ffd19784fe9971a81f5791
zigmundefreunde <saqido07@ymail.com>
2017-08-16 15:37:17 +0200

    Creation des entites de l'app

---
+++ src/Web/EntityBundle/Entity/Command.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace Web\EntityBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Command
+ *
+ * @ORM\Table(name="command")
+ * @ORM\Entity(repositoryClass="Web\EntityBundle\Repository\CommandRepository")
+ */
+class Command
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="number", type="string", length=255)
+     */
+    private $number;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="commandDate", type="datetime")
+     */
+    private $commandDate;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="amount", type="float")
+     */
+    private $amount;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="state", type="boolean")
+     */
+    private $state;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set number
+     *
+     * @param string $number
+     *
+     * @return Command
+     */
+    public function setNumber($number)
+    {
+        $this->number = $number;
+
+        return $this;
+    }
+
+    /**
+     * Get number
+     *
+     * @return string
+     */
+    public function getNumber()
+    {
+        return $this->number;
+    }
+
+    /**
+     * Set commandDate
+     *
+     * @param \DateTime $commandDate
+     *
+     * @return Command
+     */
+    public function setCommandDate($commandDate)
+    {
+        $this->commandDate = $commandDate;
+
+        return $this;
+    }
+
+    /**
+     * Get commandDate
+     *
+     * @return \DateTime
+     */
+    public function getCommandDate()
+    {
+        return $this->commandDate;
+    }
+
+    /**
+     * Set amount
+     *
+     * @param float $amount
+     *
+     * @return Command
+     */
+    public function setAmount($amount)
+    {
+        $this->amount = $amount;
+
+        return $this;
+    }
+
+    /**
+     * Get amount
+     *
+     * @return float
+     */
+    public function getAmount()
+    {
+        return $this->amount;
+    }
+
+    /**
+     * Set state
+     *
+     * @param boolean $state
+     *
+     * @return Command
+     */
+    public function setState($state)
+    {
+        $this->state = $state;
+
+        return $this;
+    }
+
+    /**
+     * Get state
+     *
+     * @return bool
+     */
+    public function getState()
+    {
+        return $this->state;
+    }
+}
+
---
+++ src/Web/EntityBundle/Entity/History.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace Web\EntityBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * History
+ *
+ * @ORM\Table(name="history")
+ * @ORM\Entity(repositoryClass="Web\EntityBundle\Repository\HistoryRepository")
+ */
+class History
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="createDate", type="date")
+     */
+    private $createDate;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="isAvailable", type="boolean")
+     */
+    private $isAvailable;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="price", type="float")
+     */
+    private $price;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set createDate
+     *
+     * @param \DateTime $createDate
+     *
+     * @return History
+     */
+    public function setCreateDate($createDate)
+    {
+        $this->createDate = $createDate;
+
+        return $this;
+    }
+
+    /**
+     * Get createDate
+     *
+     * @return \DateTime
+     */
+    public function getCreateDate()
+    {
+        return $this->createDate;
+    }
+
+    /**
+     * Set isAvailable
+     *
+     * @param boolean $isAvailable
+     *
+     * @return History
+     */
+    public function setIsAvailable($isAvailable)
+    {
+        $this->isAvailable = $isAvailable;
+
+        return $this;
+    }
+
+    /**
+     * Get isAvailable
+     *
+     * @return bool
+     */
+    public function getIsAvailable()
+    {
+        return $this->isAvailable;
+    }
+
+    /**
+     * Set price
+     *
+     * @param float $price
+     *
+     * @return History
+     */
+    public function setPrice($price)
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    /**
+     * Get price
+     *
+     * @return float
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+}
+
