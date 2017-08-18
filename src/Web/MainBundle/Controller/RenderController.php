2c9c8f2d6b552d4b610a4c62d1498dbd7ff0dbea
Danick Takam <Sdanicktakam@yahoo.fr>
2017-08-18 19:01:49 +0200

    Design de la première page

---
+++ src/Web/MainBundle/Controller/RenderController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Web\MainBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+/**
+ * @Route("/render")
+ */
+class RenderController extends Controller
+{
+    /**
+     * @Route("/category", name="category_homepage", options={"expose"=true})
+     */
+    public function categoryAction()
+    {
+        return $this->render('MainBundle:Render:category.html.twig');
+    }
+
+    /**
+     * @Route("/shop", name="shop_homepage", options={"expose"=true})
+     */
+    public function shopAction()
+    {
+        return $this->render('MainBundle:Render:shop.html.twig');
+    }
+}
